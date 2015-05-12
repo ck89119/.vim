df1a871eda5f935e7923f9e526493eed0e6a4304
ck89119 <ck89119@gmail.com>
2015-05-12 20:53:53 +0800

    modify auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -5,10 +5,10 @@
 
 git clone https://github.com:ck89119/.vim.git ~/.vim
 cd ~/.vim
-git submodule update --init
+git submodule update --init --recursive
 
 cd bundle/YouCompleteMe
-git submodule update --init --recursive
+#git submodule update --init --recursive
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev
 ./install.sh --clang-completer
